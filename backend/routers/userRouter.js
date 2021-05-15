11e7bf4851d0126110bebca353215df930b488d4
Ilias CH <iliasece16@gmail.com>
2021-05-15 23:51:11 +0300

    Copying Files Developed Before Creating the Repository

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,74 @@
+import express from 'express';
+import User from '../models/userModel';
+import expressAsyncHandler from 'express-async-handler';
+import { generateToken } from '../utils';
+
+const userRouter = express.Router(); 
+
+userRouter.get("/createadmin", 
+
+    expressAsyncHandler( async(req,res)=>{
+        try{
+            const user = new User({
+                name:'admin',
+                email:'admin@example.com',
+                password:'jsamazona',
+                isAdmin: true,
+            });
+            const createdUser = await user.save();
+            res.send(createdUser)
+        }catch(err){
+            res.status(500).send({message: err.message});
+        }
+}));
+
+userRouter.post('/signin', 
+    expressAsyncHandler (async (req,res)=>{
+        const signinUser = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+        if(!signinUser){
+            res.status(404).send({
+                message: 'Invalid Email or Password'
+            });
+        }else{
+            res.send({
+                _id: signinUser._id,
+                name: signinUser.name,
+                email: signinUser.email,
+                isAdmin: signinUser.isAdmin,
+                token: generateToken(signinUser),
+            })
+        }
+}));
+
+userRouter.post('/register', 
+    expressAsyncHandler (async (req,res)=>{
+        /*const signinUser = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })*/
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        const createdUser = await user.save();
+
+        if(!createdUser){
+            res.status(404).send({
+                message: 'Invalid User Data'
+            });
+        }else{
+            res.send({
+                _id: createdUser._id,
+                name: createdUser.name,
+                email: createdUser.email,
+                isAdmin: createdUser.isAdmin,
+                token: generateToken(createdUser),
+            })
+        }
+}));
+
+export default userRouter;
